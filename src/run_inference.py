8facb2a6d13e17a942ae8699bc467c2da5687614
Falguni Das Shuvo <dasshuvo7@gmail.com>
2024-10-04 00:28:26 +0600

    Reformat
    - Update SAVED_POLICY_PATH
    - Add num_draws and num_losses

--- src/run_inference.py
+++ src/run_inference.py
@@ -1,13 +1,17 @@
+import os
 import gymnasium as gym
 
 from src.algorithms.base_learning_algorithm import BaseLearningAlgorithm
+from src.data import DATA_DIR
 
-SAVED_POLICY_PATH = "/Users/falguni/Study/Repositories/GitHub/Schrodingers-Dealer/src/data/MCES_saved_policy.pkl"
+SAVED_POLICY_PATH = os.path.join(DATA_DIR, "MCES_saved_policy.pkl")
 
 policy = BaseLearningAlgorithm.load_policy(saved_policy_path=SAVED_POLICY_PATH)
 
 env = gym.make("Blackjack-v1", sab=True, render_mode="human")
 num_wins = 0
+num_draws = 0
+num_losses = 0
 
 for i in range(100):
     obs, info = env.reset()
@@ -24,7 +28,13 @@ for i in range(100):
 
     print(f"Game: {i} Result:{reward}")
 
-    win = 1 if reward == 1 else 0
-    num_wins += win
+    if reward == 1:
+        num_wins += 1
+    elif reward == 0:
+        num_draws += 1
+    else:
+        num_losses += 1
 
 print("Total wins: ", num_wins)
+print("Total draws: ", num_draws)
+print("Total losses: ", num_losses)
